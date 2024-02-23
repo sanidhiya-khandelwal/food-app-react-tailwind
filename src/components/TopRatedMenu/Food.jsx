1e32a6b7c06331602b66fa022e5fcb9aed6aca8c
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-02-23 12:31:03 +0530

    Created functionality for filters

--- src/components/TopRatedMenu/Food.jsx
+++ src/components/TopRatedMenu/Food.jsx
@@ -3,6 +3,11 @@ import { data } from '../../data/data';
 import FoodCard from './FoodCard';
 function Food() {
     const [foodData, setFoodData] = useState(data);
+
+    const category = (foodCategory) => setFoodData(data.filter(e => e.category === foodCategory))
+
+    const price = (foodPrice) => setFoodData(data.filter(e => e.price === foodPrice))
+
     return (
         <div>
             <div className='max-w-[1340px] m-auto mt-8'>
@@ -11,20 +16,20 @@ function Food() {
                     <div className='mb-2'>
                         <h4 className='font-bold mb-1 md:mb-4'>Filter Type</h4>
                         <div className='flex gap-2 text-orange-600 text-[0.5rem] sm:text-xl'>
-                            <button className='border-orange-600'>All</button>
-                            <button className='border-orange-600'>Burgers</button>
-                            <button className='border-orange-600'>Pizza</button>
-                            <button className='border-orange-600'>Salad</button>
-                            <button className='border-orange-600'>Chicken</button>
+                            <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white' onClick={() => setFoodData(data)}>All</button>
+                            <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white' onClick={e => category("burger")}>Burgers</button>
+                            <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white' onClick={e => category("pizza")}>Pizza</button>
+                            <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white' onClick={e => category("salad")}>Salad</button>
+                            <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white' onClick={e => category("chicken")}>Chicken</button>
                         </div>
                     </div>
                     <div>
                         <h4 className='font-bold mb-1 md:mb-3'>Filter Price</h4>
                         <div className='flex gap-2  text-orange-600 text-[0.5rem] sm:text-xl'>
-                            <button className='border-orange-600'>$</button>
-                            <button className='border-orange-600'>$$</button>
-                            <button className='border-orange-600'>$$$</button>
-                            <button className='border-orange-600'>$$$$</button>
+                            <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white' onClick={e => price("$")}>$</button>
+                            <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white' onClick={e => price("$$")}>$$</button>
+                            <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white' onClick={e => price("$$$")} >$$$</button>
+                            <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white' onClick={e => price("$$$$")}>$$$$</button>
                         </div>
                     </div>
                 </div>
@@ -36,7 +41,7 @@ function Food() {
                     ))
                 }
             </div>
-        </div>
+        </div >
     )
 }
 
